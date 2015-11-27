5ab9cc4213f176bec6689cac2327b63a9c15c2cc
Bart De Smet <bartde@microsoft.com>
2015-11-27 02:25:23 -0800

    Fixing statement lambda expression tree support.

--- src/Compilers/CSharp/Portable/Lowering/ClosureConversion/ExpressionLambdaRewriter.cs
+++ src/Compilers/CSharp/Portable/Lowering/ClosureConversion/ExpressionLambdaRewriter.cs
@@ -153,7 +153,7 @@ namespace Microsoft.CodeAnalysis.CSharp
         }
 
 
-        private BoundExpression TranslateLambdaBody(BoundNode node, ArrayBuilder<LocalSymbol> locals, ArrayBuilder<BoundExpression> initializers)
+        private BoundExpression TranslateLambdaBodyCore(BoundNode node, ArrayBuilder<LocalSymbol> locals, ArrayBuilder<BoundExpression> initializers)
         {
             var info = new LambdaCompilationInfo(this, locals, initializers);
             _lambdas.Push(info);
@@ -192,7 +192,7 @@ namespace Microsoft.CodeAnalysis.CSharp
                             case BoundKind.ReturnStatement:
                                 if (EnsureLastStatement(stmts, i))
                                 {
-                                    var result = TranslateLambdaBody(((BoundReturnStatement)stmt).ExpressionOpt, locals, initializers);
+                                    var result = TranslateLambdaBodyCore(((BoundReturnStatement)stmt).ExpressionOpt, locals, initializers);
                                     if (result != null)
                                     {
                                         return result;
@@ -230,7 +230,7 @@ namespace Microsoft.CodeAnalysis.CSharp
                 }
             }
 
-            var res = TranslateLambdaBody(block, locals, initializers);
+            var res = TranslateLambdaBodyCore(block, locals, initializers);
 
             return res;
         }
